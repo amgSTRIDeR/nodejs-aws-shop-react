875e0271a88a0daf2d469cbf5a04fb18a6144bf6
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-13 01:43:20 +0400

    feat: implement automated deployment

---
+++ cdk/cdk.ts
@@ -0,0 +1,61 @@
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+import { Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export class CdkS3CloudFront extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI");
+
+    const siteBucket = new s3.Bucket(this, "JSCCStaticBucket", {
+      bucketName: "strider-task2-bucket",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["S3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "JSCC-Distribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudFrontOAI,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      }
+    );
+
+    new s3deploy.BucketDeployment(this, "JSCC-Bucket", {
+      sources: [s3deploy.Source.asset("../dist")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+  }
+}
+
+const app = new cdk.App();
+new CdkS3CloudFront(app, "CdkS3CloudFront");
