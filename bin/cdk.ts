561664872f0f2467ec9d790741376992c97385b2
Aleksei Ovchinnikov <amgstrider@gmail.com>
2023-11-20 23:36:30 +0400

    refactor: cors allowance

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,61 +1,63 @@
-import * as cdk from "aws-cdk-lib";
-import * as s3 from "aws-cdk-lib/aws-s3";
-import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
-import * as iam from "aws-cdk-lib/aws-iam";
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as deployment from 'aws-cdk-lib/aws-s3-deployment';
 
-import { Stack } from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-export class CdkS3CloudFront extends Stack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(this, "JSCC-OAI");
-
-    const siteBucket = new s3.Bucket(this, "JSCCStaticBucket", {
-      bucketName: "task3",
-      websiteIndexDocument: "index.html",
-      publicReadAccess: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-    });
-
-    siteBucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        actions: ["S3:GetObject"],
-        resources: [siteBucket.arnForObjects("*")],
-        principals: [
-          new iam.CanonicalUserPrincipal(
-            cloudFrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
-          ),
-        ],
-      })
-    );
-
-    const distribution = new cloudfront.CloudFrontWebDistribution(
-      this,
-      "JSCC-Distribution",
-      {
-        originConfigs: [
-          {
-            s3OriginSource: {
-              s3BucketSource: siteBucket,
-              originAccessIdentity: cloudFrontOAI,
-            },
-            behaviors: [{ isDefaultBehavior: true }],
-          },
-        ],
-      }
-    );
-
-    new s3deploy.BucketDeployment(this, "JSCC-Bucket", {
-      sources: [s3deploy.Source.asset("./dist")],
-      destinationBucket: siteBucket,
-      distribution,
-      distributionPaths: ["/*"],
-    });
-  }
-}
 
 const app = new cdk.App();
-new CdkS3CloudFront(app, "CdkS3CloudFront");
+
+const stack = new cdk.Stack(app, 'S3ReactStack', {});
+
+const originAccessIdentity = new cloudfront.OriginAccessIdentity(
+  stack,
+  'S3-React-OAI'
+);
+
+const siteBucket = new s3.Bucket(stack, 'S3ReactBucket', {
+  bucketName: 'task3-frontend-bucket-st',
+  autoDeleteObjects: true,
+  publicReadAccess: false,
+  websiteIndexDocument: 'index.hml',
+  blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+
+siteBucket.addToResourcePolicy(
+  new iam.PolicyStatement({
+    actions: ['S3:GetObject'],
+    resources: [siteBucket.arnForObjects('*')],
+    principals: [
+      new iam.CanonicalUserPrincipal(
+        originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId
+      ),
+    ],
+  })
+);
+
+const clFr = new cloudfront.CloudFrontWebDistribution(
+  stack,
+  'clFr-distribution',
+  {
+    originConfigs: [
+      {
+        s3OriginSource: {
+          s3BucketSource: siteBucket,
+          originAccessIdentity: originAccessIdentity,
+        },
+        behaviors: [
+          {
+            isDefaultBehavior: true,
+          },
+        ],
+      },
+    ],
+  }
+);
+
+new deployment.BucketDeployment(stack, 'clFrDeploymebt', {
+  sources: [deployment.Source.asset('./dist')],
+  destinationBucket: siteBucket,
+  distribution: clFr,
+  distributionPaths: ['/*'],
+});
