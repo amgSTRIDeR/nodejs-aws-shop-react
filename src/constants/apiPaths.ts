539dc5a37b78412c0d2eb61c06eb44af0ad958d3
strider <amgstrider@gmail.com>
2024-01-08 16:38:01 +0400

    feat: change paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,16 @@
+// const API_PATHS = {
+//   product: "https://l3otdg6964.execute-api.eu-west-1.amazonaws.com",
+//   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+//   import: "https://r7v0mvzt98.execute-api.eu-west-1.amazonaws.com",
+//   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+//   cart: "https://0g93lqxtnh.execute-api.eu-west-1.amazonaws.com/api",
+// };
 const API_PATHS = {
-  product: "https://l3otdg6964.execute-api.eu-west-1.amazonaws.com",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://r7v0mvzt98.execute-api.eu-west-1.amazonaws.com",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "amgstrider-bff-environment.eu-west-1.elasticbeanstalk.com",
+  order: "amgstrider-bff-environment.eu-west-1.elasticbeanstalk.com",
+  import: "amgstrider-bff-environment.eu-west-1.elasticbeanstalk.com",
+  bff: "amgstrider-bff-environment.eu-west-1.elasticbeanstalk.com",
+  cart: "amgstrider-bff-environment.eu-west-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
